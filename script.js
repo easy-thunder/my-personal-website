601c5a7e5788e12a92cd9adcc9c225a99a23cdbd
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-10 15:07:22 -0700

    add Exit Button Function and add edit text details

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // modal-1 modal-2 modal-3 === modal buttons
-const modal1BTN = document.querySelector(".show-modal-1")
-const modal2BTN = document.querySelector(".show-modal-2")
-const modal3BTN = document.querySelector(".show-modal-3")
-const modal4BTN = document.querySelector(".show-modal4")
-const modal1 = document.querySelector(".modal-1")
-const modal2 = document.querySelector(".modal-2")
-const modal3 = document.querySelector(".modal-3")
-const modal4 = document.querySelector(".modal-4")
-const overlay = document.querySelector(".overlay")
+const modal0BTN = document.querySelector(".show-modal-0");
+const modal1BTN = document.querySelector(".show-modal-1");
+const modal2BTN = document.querySelector(".show-modal-2");
+const modal3BTN = document.querySelector(".show-modal-3");
+const modal0 = document.querySelector(".modal-0");
+const modal1 = document.querySelector(".modal-1");
+const modal2 = document.querySelector(".modal-2");
+const modal3 = document.querySelector(".modal-3");
+const overlay = document.querySelector(".overlay");
+const closeModal0 = document.querySelector(".close-modal0");
+const closeModal1 = document.querySelector(".close-modal1");
+const closeModal2 = document.querySelector(".close-modal2");
+const closeModal3 = document.querySelector(".close-modal3");
 
+//select button
 
+modal0BTN.addEventListener(`click`, function(){ 
+   // alert("I've been clicked")
+    modal0.classList.remove("hidden");
+    overlay.classList.remove('hidden');
+});
 modal1BTN.addEventListener(`click`, function(){ 
    // alert("I've been clicked")
     modal1.classList.remove("hidden");
@@ -25,8 +35,31 @@ modal3BTN.addEventListener(`click`, function(){
     modal3.classList.remove("hidden");
     overlay.classList.remove('hidden');
 });
-modal4BTN.addEventListener(`click`, function(){ 
-   // alert("I've been clicked")
-    modal4.classList.remove("hidden");
-    overlay.classList.remove('hidden');
-});
+//close function button
+const close0 = function(){
+    modal0.classList.add("hidden");
+    overlay.classList.add('hidden');
+};
+closeModal0.addEventListener("click", close0);
+
+
+const close1 = function(){
+    modal1.classList.add("hidden");
+    overlay.classList.add('hidden');
+};
+closeModal1.addEventListener("click", close1);
+
+
+const close2 = function(){
+    modal2.classList.add("hidden");
+    overlay.classList.add('hidden');
+};
+closeModal2.addEventListener("click", close2);
+
+
+const close3 = function(){
+    modal3.classList.add("hidden");
+    overlay.classList.add('hidden');
+};
+closeModal3.addEventListener("click", close3);
+
